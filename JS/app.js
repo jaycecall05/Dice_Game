4c4f049cb61744c24772eab63bbe5d79997eec98
Jayce <callrj5@gmail.com>
2018-06-14 21:52:15 -0600

    added the next player, defining a winner, finishing the hold btn.

--- JS/app.js
+++ JS/app.js
@@ -21,6 +21,42 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     diceDom.src = 'Assets/dice-' + dice + '.png';
 
     //    update round score IF the rolled # was not a 1
-
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //go to next player
+        nextPlayer();
+    }
 
 });
+
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    // add current score to global
+    scores[activePlayer] += roundScore;
+    //update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    //check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    } else {
+        //next player
+        nextPlayer();
+    }
+
+});
+
+function nextPlayer() {
+    //go to next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    //hide dice when player rolls 1
+    document.querySelector('.dice').style.display = 'none';
+}
